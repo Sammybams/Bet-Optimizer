cdabd0d097681fa8dd9177325d9f41377bb7b8d4
Sammybams <samuelbamgbola@gmail.com>
2023-07-24 21:18:12 +0100

    Renamed Bot to bet.

--- bot-optimizer.py
+++ bot-optimizer.py
@@ -1,2 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Bot Optimizer App", page_icon="🎯")
+st.set_page_config(page_title="Bet Optimizer App", page_icon="🎯")
+
+st.title('Bet Optimizer 🎯')
