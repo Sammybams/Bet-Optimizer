e25fef83598e03f306c88dade90d74ec30d6b63f
Sammybams <samuelbamgbola@gmail.com>
2023-07-24 21:30:17 +0100

    Imported data into script.

--- bet-optimizer.py
+++ bet-optimizer.py
@@ -3,5 +3,8 @@ st.set_page_config(page_title="Bet Optimizer App", page_icon="🎯")
 
 st.title('Bet Optimizer 🎯')
 
+import os
 from dotenv import load_dotenv
 load_dotenv('secrets.env')
+
+weights = os.environ.get("DATA")
