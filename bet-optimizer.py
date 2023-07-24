4f18139e1eba163d45336b6b07ad74971de1fe19
Sammybams <samuelbamgbola@gmail.com>
2023-07-24 22:08:51 +0100

    Added button an functinality to output stake.

--- bet-optimizer.py
+++ bet-optimizer.py
@@ -18,18 +18,35 @@ odds_a = st.number_input('Enter odds for win', min_value=1.01)
 odds_b = st.number_input('Enter odds for draw', min_value=1.01)
 odds_c = st.number_input('Enter odds for lose', min_value=1.01)
 
+if st.button("Optimize"):
 
-score = []
+    score = []
 
-for i, value in enumerate(weights):
-    w_a, w_b, w_c = value
-    a_wa = odds_a*w_a
-    b_wb = odds_b*w_b
-    c_wc = odds_c*w_c
-    
-    score.append([i, [a_wa, b_wb, c_wc]])
+    for i, value in enumerate(weights):
+        w_a, w_b, w_c = value
+        a_wa = odds_a*w_a
+        b_wb = odds_b*w_b
+        c_wc = odds_c*w_c
+        
+        score.append([i, [a_wa, b_wb, c_wc]])
 
-sorted_scores = sorted(score, key = lambda x: min(x[1]))
-best_score = sorted_scores[-1][1]
-loc_best_score = weights[sorted_scores[-1][0]]
+    sorted_scores = sorted(score, key = lambda x: min(x[1]))
+    best_score = sorted_scores[-1][1]
+    loc_best_score = weights[sorted_scores[-1][0]]
+
+    if best_score <= 1:
+        st.warning('Not optimizable for lose', icon="⚠️")
+
+        stake_win = amount*best_score[0]
+        stake_draw = amount*best_score[1]
+        stake_lose = amount*best_score[2]
+
+        output = f"""
+                | Scenario | Stake |
+                |------------------|------------------|
+                | Win | {stake_win} |
+                | Draw | {stake_draw} |
+                | Lose | {stake_lose} |
+                """
+
 
