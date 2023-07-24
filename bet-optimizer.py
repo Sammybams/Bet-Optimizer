13a9f74a6690f9d701f283ae47af84bbc2742e0b
Sammybams <samuelbamgbola@gmail.com>
2023-07-24 21:54:21 +0100

    Added commands to query weights.

--- bet-optimizer.py
+++ bet-optimizer.py
@@ -12,3 +12,22 @@ load_dotenv('secrets.env')
 weights_url = os.environ.get("DATA")
 
 weights = pd.read_parquet(weights_url).values
+
+amount = st.number_input('Enter total amount for bet', min_value=0.01)
+odds_a = st.number_input('Enter odds for win', min_value=1.0)
+odds_b = st.number_input('Enter odds for draw', min_value=1.0)
+odds_c = st.number_input('Enter odds for lose', min_value=1.0)
+
+
+score = []
+
+for i, value in enumerate(weights):
+    w_a, w_b, w_c = value
+    a_wa = odds_a*w_a
+    b_wb = odds_b*w_b
+    c_wc = odds_c*w_c
+    score_a = a_wa - (w_b + w_c)
+    score_b = b_wb - (w_a + w_c)
+    score_c = c_wc - (w_a + w_b)
+
+    score.append([i, [a_wa, b_wb, c_wc]])
